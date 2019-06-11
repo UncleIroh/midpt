af31b12f8e45f80d9345cfab0ff4e7a11831731e
Jacob Richards <jacob.richards33@gmail.com>
2019-06-10 17:01:25 -0700

    pared down to basic boilerplate

--- server/server.js
+++ server/server.js
@@ -2,18 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// uncomment the below for proxy challenge
-
-const leaderList = [
-  { name: 'Anna', id: 'a0' },
-  { name: 'Ben', id: 'b0' },
-  { name: 'Clara', id: 'c0' },
-  { name: 'David', id: 'd0' }
-];
-
-app.get('/api/leaders', (req, res) => {
-  res.send(leaderList);
-});
+// setup!
 
 if (process.env.NODE_ENV === 'production') {
   // statically serve everything in the build folder on the route '/build'
@@ -25,3 +14,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+
+// routes here
+
+app.get('/api/', (req, res) => {
+  //do stuff
+});
